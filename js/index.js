2e5cf1f509167290e92c235344d5c019502c49a6
Leesungmin98 <cjrdl22@naver.com>
2025-06-30 10:35:56 +0900

    234

--- js/index.js
+++ js/index.js
@@ -6,4 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     menuBtn.classList.toggle('active');
     menuOverlay.classList.toggle('active');
   });
-});
+}); // 메뉴 버튼
+
+function updateClock() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours === 0 ? 12 : hours; // 0시는 12로 표시
+
+  const currentTime = `KOREA | ${hours}:${minutes} ${ampm}`;
+  document.getElementById('clock').textContent = currentTime;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // 시계
+
