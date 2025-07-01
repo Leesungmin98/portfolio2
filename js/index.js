c49e9735d88fb3f4c114ce0f00d8d5adc498fe5e
Leesungmin98 <cjrdl22@naver.com>
2025-07-01 09:26:56 +0900

    324243

--- js/index.js
+++ js/index.js
@@ -216,13 +216,13 @@ function fadeIn(element) {
 ///////////////////
 
 function easeInOut(t) {
-  return t < 0.5
+  return t < 0.2
     ? 2 * t * t
     : -1 + (4 - 2 * t) * t;
 }
 
 function fadeIn(element) {
-  const duration = 300;
+  const duration = 150;
   const start = performance.now();
 
   element.style.display = "block";
